36dd12b0b43a65a1e7b8f2b3f8c41eff4956b603
Sakchhyam <khadkasakchhyam@gmail.com>
2021-08-26 15:44:49 +0545

    django

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,23 @@
-from django.urls import path, include
-from .views import home
+from django.urls import path
+from .views import home, detail, results, vote, index
+
+app_name = 'polls'
+
+#urlpatterns = [
+#    path('',home,name='home-page'),
+        # ex: /polls/5/
+#    path('specifics/<int:question_id>/', detail, name='detail'),
+    # ex: /polls/5/results/
+#    path('<int:question_id>/results/', results, name='results'),
+    # ex: /polls/5/vote/
+#    path('<int:question_id>/vote/', vote, name='vote'),
+#]
 
 urlpatterns = [
-#path('get-polls','',name='polls')
-    path('home=page',home,name='home-page')
+    path('', index, name='index'),
+    path('<int:question_id>/', detail, name='detail'),
+    path('<int:question_id>/results/', results, name='results'),
+    path('<int:question_id>/vote/', vote, name='vote'),
 ]
-
 #domain-name/polls/get-polls
-
+#path('get-polls','',name='polls')
